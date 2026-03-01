4ea216e03d841c29cdc3625b2f3ae9fdd519a3d5
Abir Howladar <abirhowlader922@gmail.com>
2026-03-01 10:44:12 +0600

    Implement prefix sum array calculation

--- calculates prefix sum array.c
+++ calculates prefix sum array.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+void prefixSum(int arr[], int n) {
+    int prefix[n];
+
+    // Calculate prefix sum
+    prefix[0] = arr[0];
+    for (int i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] + arr[i];
+    }
+
+    // Print prefix sum array
+    printf("Prefix Sum Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", prefix[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Call function
+    prefixSum(arr, n);
+
+    return 0;
+}
 
