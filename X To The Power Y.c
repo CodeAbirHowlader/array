4d7a9cf2989e34ee002556bd73c403fb13231c23
Abir Howladar <abirhowlader922@gmail.com>
2026-03-31 07:46:50 +0600

    Update X To The Power Y.c

--- X To The Power Y.c
+++ X To The Power Y.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-int calculator(int b,int e);
+
+int calculator(int b, int e)
+{
+    if(e == 0) return 1;   // base case
+    return b * calculator(b, e - 1);
+}
+
 int main()
 {
-    int b,e;
-    scanf("%d%d",&b,&e);
-    int result=calculator(b,e);
-    printf("%d",result);
-}
-int calculator(int b,int e)
-{
-//if(e==0)
-//{
-//    return 1;
-//}
-//else
-    return b*calculator(b,e-1);
+    int b, e;
+    scanf("%d%d", &b, &e);
+
+    int result = calculator(b, e);
+    printf("%d", result);
+
+    return 0;
 }
